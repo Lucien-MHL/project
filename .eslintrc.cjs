1c5ef4dc2b458f8947aeeeb6e9856367ec6b2770
Lucien <lucienengineerlife@gmail.com>
2023-11-05 18:42:03 +0800

    feat: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/semi': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
   },
 }
