dd6608cac39cea45a08752a8ba8ba212869a5fae
Lucien <lucienengineerlife@gmail.com>
2023-11-11 17:54:52 +0800

    feat: husky & write the first test

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import * as matchers from '@testing-library/jest-dom/matchers'
 import { expect } from 'vitest'
-import matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
 
 expect.extend(matchers)
