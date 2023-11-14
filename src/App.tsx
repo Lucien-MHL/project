daa089e4868df36dc87b46aeb45137be34fc7fa4
Lucien <lucienengineerlife@gmail.com>
2023-11-14 16:39:17 +0800

    feat: i18n feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import { useTranslation } from 'react-i18next'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { t } = useTranslation()
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button type='button' onClick={() => setCount((prev) => prev + 1)}>
-          count is
-          {count}
-        </button>
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <h1>{t('title')}</h1>
 }
 
 export default App
