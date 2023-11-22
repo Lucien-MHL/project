39dc8866ea7017247c762eb8d2663c3719341f5b
Lucien <lucienengineerlife@gmail.com>
2023-11-22 14:58:20 +0800

    feat: expect cli work well

--- index.js
+++ index.js
@@ -2,58 +2,55 @@
 import inquirer from 'inquirer'
 import { createSpinner } from 'nanospinner'
 
-let data, result
+let keyword
+const spin = createSpinner()
 
-async function mockFetchList(str) {
-  const searchSpinner = createSpinner(`查詢${str}中...`)
-  searchSpinner.start()
-  const sleep = (response) => new Promise((r) => setTimeout(r(response), 2000))
-  const result = sleep([
-    '慢慢等 - 官方版',
-    '翻唱 韋禮安 慢慢等',
-    '慢慢等 KTV 版本(純伴奏)',
-    '慢慢等 (歌詞)',
-    '韋禮安-慢慢等 (僅伴奏)',
-    '韋禮安現場演唱會-慢慢等',
-    '吳三不做死樂團 - 慢慢等 cover || feat: @韋禮安 ',
-  ])
-
-  if (result) {
-    searchSpinner.success({ text: `有關 ${str} 的搜尋結果` })
-    data = result
-    return
-  }
-}
-
-async function getKeyword() {
-  const keyword = await inquirer.prompt({
-    name: 'result',
-    type: 'input',
-    message: '搜尋:',
+inquirer
+  .prompt({
+    name: 'keyword',
+    message: '關鍵字查詢:',
     prefix: '🔍',
-    default: '\x1b[3m 請輸入關鍵字 \x1b[0m',
   })
-
-  return mockFetchList(keyword.result)
-}
-
-async function getIndicate(data) {
-  const indicate = await inquirer.prompt({
-    name: 'result',
-    type: 'list',
-    // message: '搜尋結果清單\n',
-    choices: data,
+  .then((res) => {
+    keyword = res.keyword
+    spin.start({ text: `查詢 ${res.keyword} 中` })
+    return new Promise((r) => {
+      setTimeout(
+        () =>
+          r([
+            '慢慢等 - 官方版',
+            '翻唱 韋禮安 慢慢等',
+            '慢慢等 KTV 版本(純伴奏)',
+            '慢慢等 (歌詞)',
+            '韋禮安-慢慢等 (僅伴奏)',
+            '韋禮安現場演唱會-慢慢等',
+            '吳三不做死樂團 - 慢慢等 cover || feat: @韋禮安 ',
+          ]),
+        2500
+      )
+    })
   })
-
-  result = indicate.result
-  return
-}
-
-async function end() {
-  console.log(`這是你選的歌曲 ➡️ <${result}>`)
-  return
-}
-
-await getKeyword()
-await getIndicate()
-await end()
+  .then(async (list) => {
+    spin.success()
+    console.clear()
+    return await inquirer.prompt({
+      name: 'result',
+      message: `有關 ${keyword} 的搜尋結果`,
+      type: 'list',
+      prefix: '🔍',
+      choices: list,
+    })
+  })
+  .then((query) => {
+    spin.start({ text: `載入<${query.result}>` })
+    return new Promise((r) => {
+      setTimeout(() => {
+        r(query.result)
+      }, 2000)
+    })
+  })
+  .then((audio) => {
+    spin.success()
+    console.clear()
+    console.log(`正位您撥放 ${audio}`)
+  })
