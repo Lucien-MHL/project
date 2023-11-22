6fb0ff2a141b437474d49d79e3caa9da04d011a1
Lucien <lucienengineerlife@gmail.com>
2023-11-23 00:27:58 +0800

    fest: play-dl

--- index.js
+++ index.js
@@ -1,56 +1,45 @@
 #!/usr/bin/env node
+import play from 'play-dl'
 import inquirer from 'inquirer'
 import { createSpinner } from 'nanospinner'
-
-let keyword
 const spin = createSpinner()
 
 inquirer
   .prompt({
     name: 'keyword',
     message: '關鍵字查詢:',
+    type: 'input',
     prefix: '🔍',
   })
-  .then((res) => {
-    keyword = res.keyword
+  .then(async (res) => {
     spin.start({ text: `查詢 ${res.keyword} 中` })
-    return new Promise((r) => {
-      setTimeout(
-        () =>
-          r([
-            '慢慢等 - 官方版',
-            '翻唱 韋禮安 慢慢等',
-            '慢慢等 KTV 版本(純伴奏)',
-            '慢慢等 (歌詞)',
-            '韋禮安-慢慢等 (僅伴奏)',
-            '韋禮安現場演唱會-慢慢等',
-            '吳三不做死樂團 - 慢慢等 cover || feat: @韋禮安 ',
-          ]),
-        2500
-      )
-    })
+    const list = await play.search(res.keyword)
+    return { list, key: res.keyword }
   })
-  .then(async (list) => {
+  .then(async (data) => {
     spin.success()
     console.clear()
-    return await inquirer.prompt({
+    const choose = await inquirer.prompt({
       name: 'result',
-      message: `有關 ${keyword} 的搜尋結果`,
+      message: `有關 ${data.key} 的搜尋結果`,
       type: 'list',
       prefix: '🔍',
-      choices: list,
+      choices: data.list.map((item) => item.title),
     })
+    return {
+      info: data.list.find((item) => item.title === choose.result),
+      name: choose.result,
+    }
   })
-  .then((query) => {
-    spin.start({ text: `載入<${query.result}>` })
-    return new Promise((r) => {
-      setTimeout(() => {
-        r(query.result)
-      }, 2000)
-    })
+  .then(async (info) => {
+    spin.start({ text: `載入<${info.name}>` })
+    const stream = await play.stream(info.info.url)
+    return { stream, name: info.name }
   })
-  .then((audio) => {
+  .then((result) => {
     spin.success()
-    console.clear()
-    console.log(`正位您撥放 ${audio}`)
+    console.log(result.stream)
+  })
+  .catch((err) => {
+    throw new Error(err)
   })
